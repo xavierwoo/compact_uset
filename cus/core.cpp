e06e09f97e45dcc946bdad505222a48fcce08fe5
xavierwoo <xavier.woo@gmail.com>
2025-03-20 17:28:21 +0800

    Add checking for the same values

--- cus/core.cpp
+++ cus/core.cpp
@@ -15,6 +15,9 @@ void compact_uset::it_works() {
     assert(set.get_hash_list()[3] == 0);
     assert(set.get_hash_list()[1] == 1);
 
+    set.insert(1);
+    assert(set.size() == 2);
+
     set.remove(3);
     assert(set.get_data_list()[0] == 1);
     assert(set.get_hash_list()[1] == 0);
